8d6e6013efc26125541d35bf24807e1e6a40e133
ImpacT. <100585069+Z4rga@users.noreply.github.com>
2023-10-09 21:22:14 +0100

    first commit

---
+++ src/Repository/BraveKangarooRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\BraveKangaroo;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<BraveKangaroo>
+ *
+ * @method BraveKangaroo|null find($id, $lockMode = null, $lockVersion = null)
+ * @method BraveKangaroo|null findOneBy(array $criteria, array $orderBy = null)
+ * @method BraveKangaroo[]    findAll()
+ * @method BraveKangaroo[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class BraveKangarooRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, BraveKangaroo::class);
+    }
+
+//    /**
+//     * @return BraveKangaroo[] Returns an array of BraveKangaroo objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('b')
+//            ->andWhere('b.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('b.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?BraveKangaroo
+//    {
+//        return $this->createQueryBuilder('b')
+//            ->andWhere('b.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
